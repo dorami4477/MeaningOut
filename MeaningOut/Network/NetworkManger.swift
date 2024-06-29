080f3b52f9b70d8f36e423febbcf1c9499d74905
dahyun <dahyun4477@gmail.com>
2024-06-29 19:03:50 +0900

    refactor : 네트워크 통신 상태코드에 따른 에러처리

--- MeaningOut/Network/NetworkManger.swift
+++ MeaningOut/Network/NetworkManger.swift
@@ -15,25 +15,51 @@ class NetworkManger{
     static let shared = NetworkManger()
     private init(){}
     
+    enum ErrorCode:Error{
+        case invalidRequest
+        case invalidAuth
+        case invalidURL
+        case overRequest
+        case serverError
+    }
     
-    func callRequest(api:ShoppingAPI, compeltion: @escaping (Result<Shopping, Error>) -> Void){
-
+    func callRequest(api:ShoppingAPI, compeltion: @escaping (Result<Shopping, ErrorCode>) -> Void){
+        
         AF.request(api.endPoint,
                    method: api.method,
                    parameters: api.parameter,
                    encoding: URLEncoding(destination: .queryString),
                    headers: api.header)
-            .validate(statusCode: 200..<500)
-            .responseDecodable(of: Shopping.self){ response in
+        .validate(statusCode: 200..<500)
+        .responseDecodable(of: Shopping.self){ response in
+            
             switch response.result{
             case .success(let value):
                 compeltion(.success(value))
-                
             case .failure(let error):
-                print(error)
-                compeltion(.failure(error))
+                let statusCode = response.response?.statusCode
+                switch statusCode {
+                case 400:
+                    print("error:요청 변수 확인")
+                    compeltion(.failure(.invalidRequest))
+                case 401:
+                    print("error:인증 실패")
+                    compeltion(.failure(.invalidAuth))
+                case 404:
+                    print("error:API 없음")
+                    compeltion(.failure(.invalidURL))
+                case 429:
+                    print("error:호출 한도 초과 오류")
+                    compeltion(.failure(.overRequest))
+                case 500:
+                    print("error:서버 오류")
+                    compeltion(.failure(.serverError))
+                default:
+                    print("error:그 외 에러")
+                    compeltion(.failure(.invalidRequest))
+                }
             }
         }
+        
     }
 }
-
